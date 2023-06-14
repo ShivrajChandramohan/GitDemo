1aa75cc402ad5268610db39f0723e582d737e0e8
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-06-14 09:14:40 +0530

    [Shivraj] Add: UC1-Demo

---
+++ NewDemo/NewDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GitDemo gitDemo = new GitDemo();
+            
+            gitDemo.Demo();
+            
+            Console.ReadLine();
+        }
+    }
+}
